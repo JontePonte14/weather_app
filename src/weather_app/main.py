9f3e4e586755baf3299cf9f2fe762e031fc7fd5f
Jonathan <jonathan.vonbergen@gmail.com>
2025-06-07 15:33:07 +0200

    Basic main.py made

--- src/weather_app/main.py
+++ src/weather_app/main.py
@@ -1,9 +1,27 @@
+from utils import *
+from api import *
+
+
 def main():
-    print("Hello world!")
+    print("Hello! Insert below what city you want weather info on")
+    cityName = input()
+    print("Fetching...")
+    weather_info = get_weather_data(cityName)
+
+    print("")
+    if weather_info:
+        print(f"City: {weather_info['name']}")
+        print(f"Temperature: {weather_info['main']['temp']} C degrees")
+        print(f"Description: {weather_info['weather'][0]['description'].capitalize()}")
+        print(f"Wind speed: {weather_info['wind']['speed']} m/s")
+    else:
+        print("Failed, please try again later")
     # 1. Get input (e.g. a city name)
     # 2. Fetch weather data via your API function
     # 3. Use utility functions to extract what you want
     # 4. Print it nicely
+
+
 
 if __name__ == "__main__":
     main()
